e78f45289e0194eae332b7bacaa623d26f6ecadd
git-innit <187617015+git-innit@users.noreply.github.com>
2024-11-27 13:26:50 +0100

    Add Aufgabe 3

---
+++ Aufgabe-03.ps1
@@ -0,0 +1,11 @@
+﻿Set-Location -Path C:\
+Get-ChildItem
+New-Item -Name "mytest" -ItemType directory
+Get-ChildItem -Name m* -Recurse
+Write-Host $pshome
+Set-Location -Path C:\mytest
+"my fancy text file :-)" | Set-Content -Path .\testfile.txt
+Get-Date
+Clear-Host
+Get-Command -Name *get-c*
+Get-ComputerInfo
