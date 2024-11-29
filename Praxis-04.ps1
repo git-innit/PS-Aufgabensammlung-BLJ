bcd262a9b1fd283453f3ea031c3790d57c8316d5
git-innit <187617015+git-innit@users.noreply.github.com>
2024-11-29 15:46:22 +0100

    Add Praxis 4

---
+++ Praxis-04.ps1
@@ -0,0 +1,8 @@
+﻿$language = "de"
+$count = "2"
+$response = Invoke-RestMethod -Uri "https://witzapi.de/api/joke/?limit=$count&language=$language" -Method Get
+$i = 1
+foreach ($text in $response) {
+Write-Host "[$i] $($text.text)"
+$i++
+}
